e5d8e7049852148124339e312c87f8706af446c4
steve <steve@localhost>
2019-09-09 01:31:33 +0300

    Add shake handler

--- DecisionMaker/js/dmaker.js
+++ DecisionMaker/js/dmaker.js
@@ -6,3 +6,18 @@ answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes - defi
 function makeDecision() {
     el('decision-output').innerHTML = answers[Math.floor(Math.random()*answers.length)]
 }
+
+//listen to shake event
+let shakeEvent = new Shake({threshold: 15});
+shakeEvent.start();
+window.addEventListener('shake', function(){
+    makeDecision()
+}, false);
+
+//stop listening
+function stopShake(){
+    shakeEvent.stop();
+}
+
+//check if shake is supported or not.
+if(!("ondevicemotion" in window)){alert("Not Supported");}
