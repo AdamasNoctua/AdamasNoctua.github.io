3c780856b377f5009ad55c094c48e3b58f8064e0
steve <steve@localhost>
2019-09-10 02:41:48 +0300

    Decision maker style prototype

--- DecisionMaker/js/dmaker.js
+++ DecisionMaker/js/dmaker.js
@@ -3,12 +3,29 @@ answers = ['It is certain', 'It is decidedly so', 'Without a doubt', 'Yes - defi
 'Reply hazy, try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and ask again',
 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook not so good', 'Very doubtful'];
 
+let canClick = true;
+
+/**
+ * Make decision
+ */
 function makeDecision() {
-    el('decision-output').innerHTML = answers[Math.floor(Math.random()*answers.length)]
+    if(canClick){
+        let interval = 0;
+        canClick = false;
+        if(el('triangle').style.opacity === '1'){
+            interval = 3000;
+            el('triangle').style.opacity = '0';
+        }
+        setTimeout(()=>{
+            el('decision-output').innerHTML = answers[Math.floor(Math.random()*answers.length)];
+            el('triangle').style.opacity = '1';
+            canClick = true;
+        }, interval);
+    }
 }
 
 //listen to shake event
-let shakeEvent = new Shake({threshold: 15});
+let shakeEvent = new Shake({threshold: 30});
 shakeEvent.start();
 window.addEventListener('shake', function(){
     makeDecision()
@@ -20,4 +37,4 @@ function stopShake(){
 }
 
 //check if shake is supported or not.
-if(!("ondevicemotion" in window)){alert("Not Supported");}
+if(!("ondevicemotion" in window)){console.log("Warning: Shaking is not Supported");}
